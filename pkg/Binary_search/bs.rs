538ba604037202394c6841a744938868d9b0e79c
nt2311-vn <nguyentoan231196@gmail.com>
2024-09-22 09:55:33 +0700

    Solving with rust

--- pkg/Binary_search/bs.rs
+++ pkg/Binary_search/bs.rs
@@ -1,6 +1,19 @@
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        let mut l = 0
-        let mut r = nums.len() as i32 -1
+        let mut l = 0;
+        let mut r = nums.len() as i32 -1;
+
+        while l <= r {
+            let mid = l + (r-l) /2;
+            if nums[mid as usize] == target {
+                return mid;
+            } else if nums[mid as usize] < target {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return -1;
     }
 }
