2cf85e637acc27f8de67e287d7514e86add0b579
nt2311-vn <nguyentoan231196@gmail.com>
2024-09-24 11:38:09 +0700

    another apporach no need for extra function overhead

--- pkg/CountNegatives/count.go
+++ pkg/CountNegatives/count.go
@@ -1,24 +1,17 @@
 package countnegatives
 
-func reverseBinarySearch(arr []int) int {
-	l, r := 0, len(arr)-1
-	for l < r {
-		m := l + (r-l)/2
-		if arr[m] >= 0 {
-			l = m + 1
-		} else {
-			r = m
-		}
-	}
-
-	return len(arr[l:])
-}
-
 func countNegatives(grid [][]int) int {
 	result := 0
 
 	for _, arr := range grid {
-		result += reverseBinarySearch(arr)
+		for i := len(arr) - 1; i >= 0; i-- {
+			if arr[i] < 0 {
+				result++
+			} else {
+				break
+			}
+		}
 	}
+
 	return result
 }
